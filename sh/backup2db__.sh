26d90a31273e6edf6826d0d7d3139665bb0e0519
Finley <galoislee@yahoo.com.tw>
2021-12-13 08:14:50 +0800

    Create backup2db__.sh
    db備份打包至NAS

---
+++ sh/backup2db__.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+cd /home/bkup__ 
+#echo 'Backup ALL Database' 
+date 
+for db in calendar counseling coursehist netaccess socialgrouplink stucis sylla$ 
+do 
+        sqlfile="/home/bkup__/db_back/$db`date +%Y%m%d%H%M`.sql" 
+        mysqldump -u bkup__ -p1o4zp4 $db > $sqlfile 
+        FN="/home/bkup__/$db`date +%Y%m%d%H%M`_enc.sql.tar.gz" 
+        #壓縮打包並加密 
+        tar -cpz $sqlfile | openssl enc -e -aes256 -k ndb__pwd -out $FN 
+        #傳輸 
+        #壓縮傳輸後移除原檔 
+        scp -P 6561 $FN dbmgr.isrcttu.net:/backup/ndb__/$db 
+        rm -f $FN 
+        echo "$db 已遠端備份至 NAS" 
+done 
