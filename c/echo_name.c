2a8e1d45296354c6d2a7d4e505b3e50925d65fe9
Finley <fanli@gm.ttu.edu.tw>
2022-01-05 23:23:18 +0800

    echo

---
+++ c/echo_name.c
@@ -0,0 +1,26 @@
+/**
+ * Program Name: example.c
+ * Subject: echo name
+ * Begin Date: 2022/01/05
+ * Modify Date: --/--/--
+ * Toolkit: gcc 9.3.0, Atom 1.58.0
+ * Author: Finley
+ *         Computer Center - System Development & Network Division
+ *         Tatung University
+ */
+#include <stdio.h>
+
+int main() {
+
+/*add printf show input data*/
+    printf("  __ _       _                _____             _     _ \n");
+    printf(" / _(_)_ __ | | ___ _   _    |  ___|_ _ _ __   | |   (_)\n");
+    printf("| |_| |  _  | |/ _   | | |   | |_ / _  |  _  | | |   | |\n");
+    printf("|  _| | | | | |  __/ |_| |_  |  _| (_| | | | | | |___| |\n");
+    printf("|_| |_|_| |_|_||___|___  ( ) |_|  |__ _|_| |_| |_____|_|\n");
+    printf("                    |___/|/\n");
+    printf("********************************************************\n");
+    printf("* E-mail_fnali@gm.ttu.edu.tw, finley@ms.tsh.ttu.edu.tw *\n");
+    printf("* Website_https://finleyli.medium.com/                 *\n");
+    printf("********************************************************\n");
+}
