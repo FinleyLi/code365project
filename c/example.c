ac26c1fced539075880f994f1f3a07a0afcb1614
Finley <fanli@gm.ttu.edu.tw>
2022-01-05 23:19:46 +0800

    echo finley

---
+++ c/example.c
@@ -0,0 +1,33 @@
+/**
+ * Program Name: 3-4_input.c
+ * Subject: Scanf keying data
+ * Begin Date: 2021/11/30
+ * Modify Date: --/--/--
+ * Toolkit: gcc 9.3.0, Code::Blocks 20.03
+ * Author: Finley
+ *         Computer Center - System Development & Network Division
+ *         Tatung University
+ */
+#include <stdio.h>
+
+int main() {
+
+    int varInt;
+    long varLong;
+    float varFloat;
+    double varDouble;
+    //scanf("%d%ld%f%lf", &varInt, &varLong, &varFloat, &varDouble);
+/*add printf show input data*/
+    //printf("varInt=%d, varLong=%ld, varFloat=%f, varDouble=%lf\n", varInt, varLong, varFloat, varDouble);
+/*fingle*/
+    printf("  __ _       _                _____             _     _ \n");
+    printf(" / _(_)_ __ | | ___ _   _    |  ___|_ _ _ __   | |   (_)\n");
+    printf("| |_| |  _  | |/ _   | | |   | |_ / _  |  _  | | |   | |\n");
+    printf("|  _| | | | | |  __/ |_| |_  |  _| (_| | | | | | |___| |\n");
+    printf("|_| |_|_| |_|_||___|___  ( ) |_|  |__ _|_| |_| |_____|_|\n");
+    printf("                    |___/|/\n");
+    printf("********************************************************\n");
+    printf("* E-mail_fnali@gm.ttu.edu.tw, finley@ms.tsh.ttu.edu.tw *\n");
+    printf("* Website_https://finleyli.medium.com/                 *\n");
+    printf("********************************************************\n");
+}
